538c66b2d4a443a0611889844c0a49519825f285
ビザンコムマック０７ <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-09-19 11:00:28 +0900

    Initial Commit

---
+++ HOMEO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HOMEO
+//
+//  Created by ビザンコムマック０７ on 2014/09/19.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HOMEO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HOMEO
+//
+//  Created by ビザンコムマック０７ on 2014/09/19.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
